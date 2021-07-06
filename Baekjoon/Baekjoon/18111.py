02e90907ed2dee7a1af3aaaceee6f8e72af3ec94
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2021-07-06 14:22:43 +0900

    Add files via upload

--- Baekjoon/Baekjoon/18111.py
+++ Baekjoon/Baekjoon/18111.py
@@ -1,26 +1,21 @@
-def solve(n,cnt):
-    while True:
-        for i in range(n-1):
-            if lst[i] > lst[i+1]:
-                lst[i] += 1
-                cnt += 1
-            elif lst[i] < lst[i+1]:
-                lst[i] -= 1
-                cnt += 2
-
-    return cnt,lst[0][0]
-
-n,m,b = map(int,input().split())
-
-lst_ = []
-lst = []
-for i in range(n):  
-    lst_.append(list(map(int,input().split())))
-
-for i in lst_:
-    for j in i:
-        lst.append(j)
-cnt = 0
-
-cnt,top = solve(n,cnt)
-print(cnt,top)
+import sys
+input = sys.stdin.readline
+n, m, b = map(int,input().split())
+lst = [list(map(int,input().split())) for _ in range(n)]
+dic = {}
+for h in range(257):
+    bot = top = 0
+    for i in range(n):
+        for j in range(m):
+            if lst[i][j] > h:
+                top += lst[i][j] - h
+            else:
+                bot += h - lst[i][j]
+    
+    if b + top < bot:
+        continue
+    time = bot + (top *2)
+    dic[time] = h
+t = min(dic)
+max_h = dic.get(t)
+print(t,max_h)

7d7188c9720a7ada814545eb7ce50a7d43923225
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-07-01 01:55:11 +0900

    [Silver I] Title: 세훈이의 선물가게, Time: 480 ms, Memory: 135792 KB -BaekjoonHub

--- 백준/Silver/17225. 세훈이의 선물가게/세훈이의 선물가게.py
+++ 백준/Silver/17225. 세훈이의 선물가게/세훈이의 선물가게.py
@@ -1,43 +1,33 @@
+import heapq
+
 a, b, n= map(int,input().split())
-a_li= []
-b_li= []
+li= []
+end_a, end_b= 0, 0
 
 for _ in range(n):
     t, c, m= input().split()
     t,m = int(t), int(m)
     if c== 'B':
-        if a_li:
-            if a_li[-1]+a > t:
-                t = a_li[-1]+a
+        t = max(end_a,t)
         for i in range(m):
-            a_li.append(t+(a*i))
+            heapq.heappush(li,[t,'B'])
+            t+= a
+            end_a= t
     else:
-        if b_li:
-            if b_li[-1]+b > t:
-                t = b_li[-1]+b
+        t = max(end_b,t)
         for i in range(m):
-            b_li.append(t+(b*i))
+            start= t+(b*i)
+            heapq.heappush(li,[start,'R'])
+            end_b =start+b
 
-number = 1
-a_do= []
-b_do= []
-for i in range(1,max(max(a_li),max(b_li))+1):
-    while True:
-        if a_li and a_li[0] == i:
-            a_do.append(number)
-            a_li.pop(0)
-            number+= 1
-        else:
-            break
-    while True:
-        if b_li and b_li[0] == i:
-            b_do.append(number)
-            b_li.pop(0)
-            number+=1
-        else:
-            break
+a_do, b_do= [], []
 
-print(len(a_do))
-print(*a_do)
-print(len(b_do))
-print(*b_do)
+for i in range(1,len(li)+1):
+    time, color= heapq.heappop(li)
+    if color == 'B':
+        a_do.append(i)
+    else:
+        b_do.append(i)
+
+print(len(a_do)); print(*a_do)
+print(len(b_do)); print(*b_do)

141f620e73f02f578481f2587cc7163f017c17c0
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-01-09 23:27:11 +0900

    [Silver I] Title: 추월, Time: 420 ms, Memory: 18476 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.kt
+++ 백준/Silver/2002. 추월/추월.kt
@@ -1,8 +1,10 @@
-package heejik.`14week`
+package heejik.`17week`
 
 /*
 * 앞에 있는 차 중에 원래 앞에 있던 애들이 아니고 이미 추월한 애가 아닌
 애의 숫자들의 합
+*
+* 나왔더니 원래 앞에 있던 차들 중 하나라도 없으면 난 추월했어~
 *
 */
 class 추월 {
@@ -13,7 +15,7 @@ class 추월 {
         val carsWithFrontCars = mutableMapOf<String, List<String>>()
         val inFrontCars = mutableListOf<String>()
         val outFrontCars = mutableListOf<String>()
-        val overtakenCars = mutableListOf<String>()
+        var cnt = 0
 
         repeat(n) {
             val inCar = readln()
@@ -23,11 +25,17 @@ class 추월 {
 
         repeat(n) {
             val outCar = readln()
-            overtakenCars.addAll(outFrontCars.filter { it !in carsWithFrontCars[outCar]!! && it !in overtakenCars })
+            carsWithFrontCars[outCar]!!.forEach {
+                if (outFrontCars.contains(it).not()) {
+                    cnt++
+                    outFrontCars.add(outCar)
+                    return@repeat
+                }
+            }
             outFrontCars.add(outCar)
         }
 
-        println(overtakenCars.size)
+        println(cnt)
     }
 }
 

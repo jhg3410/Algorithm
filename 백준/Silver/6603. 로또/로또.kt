4ef5b6f48641d2b0786c0a958845b9f807580949
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-08-20 20:47:54 +0900

    [Silver II] Title: 로또, Time: 200 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.kt
@@ -0,0 +1,44 @@
+package heejik.`42week`
+
+class 로또 {
+    var answer = StringBuilder()
+
+    fun solve() {
+        getInput()
+    }
+
+    private fun getInput() {
+        val inputs = mutableListOf<List<Int>>()
+
+        while (true) {
+            val numbers = readln().split(' ').map { it.toInt() }.drop(1)
+            if (numbers.isEmpty()) break
+            inputs.add(numbers)
+        }
+
+        inputs.forEachIndexed { index, ints ->
+            pickNumber(ints)
+            if (index != inputs.size - 1) {
+                answer.appendLine()
+            }
+        }
+
+        print(answer)
+    }
+
+    private fun pickNumber(numbers: List<Int>, pickIndex: Int = 0, storeNumbers: List<Int> = emptyList()) {
+        if (storeNumbers.size == 6) {
+            answer.appendLine(storeNumbers.joinToString(" "))
+            return
+        }
+
+        for (i in pickIndex until numbers.size) {
+            pickNumber(numbers = numbers, pickIndex = i + 1, storeNumbers = storeNumbers.plus(numbers[i]))
+        }
+    }
+}
+
+
+fun main() {
+    로또().solve()
+}

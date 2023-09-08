c09046e8e17a44ea7c644b44680fc11e0b2d8191
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-09-08 16:34:11 +0900

    [Gold V] Title: CCW, Time: 136 ms, Memory: 21416 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.kt
@@ -0,0 +1,28 @@
+package heejik.`44week`
+
+class CCW {
+
+    data class Vector(
+        val x :Int,
+        val y: Int
+    )
+    fun solve() {
+        val (p1X, p1Y) = readln().split(' ').map { it.toInt() }
+        val (p2X, p2Y) = readln().split(' ').map { it.toInt() }
+        val (p3X, p3Y) = readln().split(' ').map { it.toInt() }
+
+        val vectorP1P2 = Vector(p2X - p1X, p2Y - p1Y)
+        val vectorP1P3 = Vector(p3X - p1X, p3Y - p1Y)
+
+        (vectorP1P2.x * vectorP1P3.y - vectorP1P3.x * vectorP1P2.y).run {
+            if (this < 0) print(-1)
+            if (this == 0) print(0)
+            if (this > 0) print(1)
+        }
+    }
+}
+
+
+fun main() {
+    CCW().solve()
+}

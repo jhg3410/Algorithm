93f417788c6d916bd3cb41cbb07d5ad86fe5d4fe
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-10-08 11:20:22 +0900

    [level 3] Title: 입국심사, Time: 13.37 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(n: Int, times: IntArray): Long {
+        var minTime : Long = 1L
+        var maxTime : Long = times.maxOrNull()!!.toLong() * n
+        
+        while (minTime <= maxTime) {
+            val midTime = (minTime + maxTime) / 2
+            
+            var count = 0L
+            times.forEach { time -> count += midTime / time }
+            
+            if (count >= n) {
+                maxTime = midTime - 1
+            } else {
+                minTime = midTime + 1
+            }
+        }
+        return minTime
+    }
+}

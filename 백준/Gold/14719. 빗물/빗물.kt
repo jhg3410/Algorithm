c9b0d118767b3dbbf622b2b2a9c6f8f17e4a05ec
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-11-09 19:36:16 +0900

    [Gold V] Title: 빗물, Time: 276 ms, Memory: 25920 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.kt
+++ 백준/Gold/14719. 빗물/빗물.kt
@@ -1,4 +1,4 @@
-package heejik.`31week`
+package heejik.`49week`
 
 import kotlin.math.min
 import kotlin.properties.Delegates
@@ -7,32 +7,44 @@ class 빗물 {
 
     var h by Delegates.notNull<Int>()
     var w by Delegates.notNull<Int>()
-    private lateinit var blocks: List<Int>
+    private lateinit var heights: List<Int>
+
     fun solve() {
+        setting()
+        getRainWater().also { rainWater ->
+            print(rainWater)
+        }
+    }
+
+    private fun setting() {
         readln().split(' ').map { it.toInt() }.run {
             h = this[0]
             w = this[1]
         }
-        blocks = readln().split(' ').map { it.toInt() }
-        println(getTotalMountOfRainwater())
+        heights = readln().split(' ').map { it.toInt() }
     }
 
-    private fun getTotalMountOfRainwater(): Int {
-        var s = 0
-        blocks.forEachIndexed { index, height ->
-            val leftMax = blocks.filterIndexed { subIndex, i ->
-                subIndex < index
-            }.maxOrNull() ?: 0
-            val rightMax = blocks.filterIndexed { subIndex, i ->
-                subIndex > index
-            }.maxOrNull() ?: 0
-            (min(leftMax, rightMax) - height).run {
-                if (this > 0) s += this
+    private fun getRainWater(): Int {
+        var rainWater = 0
+
+        heights.forEachIndexed { index, height ->
+            if (index == 0 || index == heights.size - 1) return@forEachIndexed
+
+            val minHeightOfBothSideMaxHeight = min(
+                heights.subList(0, index + 1).max() ?: 0,
+                heights.subList(index + 1, heights.size).max() ?: 0
+            )
+            
+            with(minHeightOfBothSideMaxHeight - height) {
+                if (this > 0) rainWater += this
             }
+
         }
-        return s
+
+        return rainWater
     }
 }
+
 
 fun main() {
     빗물().solve()

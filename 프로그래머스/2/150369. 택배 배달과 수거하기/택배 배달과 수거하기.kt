6912bffed9066fe83e0657fd45b3c63da7f3a4e1
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-02-28 20:54:17 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 6.45 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.kt
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.kt
@@ -1,43 +1,22 @@
 import kotlin.math.*
 
 class Solution {
-    fun solution(cap: Int, n: Int, deliveries: IntArray, pickups: IntArray): Long {
+   fun solution(cap: Int, n: Int, deliveries: IntArray, pickups: IntArray): Long {
+
         var distance = 0L
-        var boxCount = 0
+        var saveDelivery = 0
+        var savePickup = 0
 
-        while (true) {
-            val deliveryPoint = deliveries.indexOfLast { it != 0 }
-            val pickupPoint = pickups.indexOfLast { it != 0 }
-
-            if (deliveryPoint == -1 && pickupPoint == -1) break
-
-            distance += max(deliveryPoint + 1, pickupPoint + 1) * 2
-
-            boxCount = cap
-            for (i in deliveryPoint downTo 0) {
-                if (deliveries[i] == 0) continue
-                
-                if (deliveries[i] > boxCount) {
-                    deliveries[i] -= boxCount
-                    break
-                } else {
-                    boxCount -= deliveries[i]
-                    deliveries[i] = 0
-                }
+        for (i in n - 1 downTo 0) {
+            var visit = 0
+            while (visit * cap + saveDelivery < deliveries[i] || visit * cap + savePickup < pickups[i]) {
+                visit++
             }
-            boxCount = 0
-            
-            for (i in pickupPoint downTo 0) {
-                if (pickups[i] == 0) continue
-                
-                if (pickups[i] > cap - boxCount) {
-                    pickups[i] -= cap - boxCount
-                    break
-                } else {
-                    boxCount += pickups[i]
-                    pickups[i] = 0
-                }
-            }
+
+            saveDelivery = visit * cap + saveDelivery - deliveries[i]
+            savePickup = visit * cap + savePickup - pickups[i]
+
+            distance += 2 * visit * (i + 1)
         }
 
         return distance

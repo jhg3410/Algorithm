a898237c9721b866edbe87b63e05ed2cded5c6c2
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-03-20 01:41:05 +0900

    [level 3] Title: 베스트앨범, Time: 27.05 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.kt
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.kt
@@ -1,18 +1,12 @@
 class Solution {
     fun solution(genres: Array<String>, plays: IntArray): IntArray {
         var answer = intArrayOf()
-        val genresMap = mutableMapOf<String, MutableList<Int>>()
-
-        for ((idx, genre) in genres.withIndex()) {
-            if (genre !in genresMap) genresMap[genre] = mutableListOf()
-            genresMap[genre]?.add(idx)
-        }
-        genresMap.toList().sortedByDescending { it.second.sumOf { plays[it] } }.forEach first@ {
-            var count = 0
-            it.second.sortedByDescending { plays[it] }.forEach second@ {
-                count++
-                answer += it
-                if (count == 2) return@first
+        val genresMap = genres.indices.groupBy { genres[it] }
+        
+        genresMap.toList().sortedByDescending { it.second.sumOf { plays[it] } }.forEach first@{
+            it.second.sortedByDescending { plays[it] }.forEachIndexed { idx, id ->
+                answer += id
+                if (idx == 1) return@first
             }
         }
 

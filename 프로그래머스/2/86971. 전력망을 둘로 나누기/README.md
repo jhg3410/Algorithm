6debec033392c9d2fed2e6e7d1cbddaaf81a373a
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-03-20 18:49:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=python3) 
 
 ### 성능 요약
 
-메모리: 65.1 MB, 시간: 40.59 ms
+메모리: 10.2 MB, 시간: 1.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 18:14:34
+2024년 03월 20일 18:49:26
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,33 @@
+answer = 999999999999
+
+def solution(n, wires):
+    relations = [[False for _ in range(n)] for _ in range(n)]
+    
+    for wire in wires:
+        x, y = map(lambda x: x - 1, wire)
+        relations[x][y] = True
+        relations[y][x] = True
+
+    print(relations)
+    dfs(n, 0, relations)
+    
+    return answer
+
+
+def dfs(n, idx, relations):
+    global answer
+    count = 1
+    
+    for i in range(n):
+        if relations[idx][i]:
+            relations[idx][i] = False
+            relations[i][idx] = False
+            count += dfs(n, i, relations)
+            relations[idx][i] = True
+            relations[i][idx] = True
+    
+    answer = min(answer, abs(n - count - count))
+
+    return count
+
+

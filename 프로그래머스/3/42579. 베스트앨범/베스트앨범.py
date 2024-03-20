860ce0edc557940f6c7e2ae645b26d033e173619
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-03-20 17:22:49 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,16 +1,15 @@
 
 def solution(genres, plays):
     answer = []
-
     genresDict = dict()
+
     for i in range(len(genres)):
         if genres[i] not in genresDict.keys(): genresDict[genres[i]] = []
         genresDict[genres[i]].append(i)
 
-    genresDict_sorted = {k: v for k, v in sorted(genresDict.items(), key = lambda x: sum(map(lambda q: plays[q], x[1])), reverse= True)}
+    genresDict_sorted = sorted(genresDict.values(), key = lambda x: sum(map(lambda q: plays[q], x)), reverse= True)
 
-
-    for values in genresDict_sorted.values():
+    for values in genresDict_sorted:
         answer.extend(sorted(values, key = lambda x: plays[x], reverse= True)[:2])
 
     return answer

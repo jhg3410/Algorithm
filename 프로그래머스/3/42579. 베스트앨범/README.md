310c8328ecd9ec4a6ebee89426f7bbcbf9ef9444
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-03-20 17:24:41 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,6 +1,6 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 17:22:47
+2024년 03월 20일 17:24:39
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,10 +1,10 @@
+from collections import defaultdict
 
 def solution(genres, plays):
     answer = []
-    genresDict = dict()
+    genresDict = defaultdict(list)
 
     for i in range(len(genres)):
-        if genres[i] not in genresDict.keys(): genresDict[genres[i]] = []
         genresDict[genres[i]].append(i)
 
     genresDict_sorted = sorted(genresDict.values(), key = lambda x: sum(map(lambda q: plays[q], x)), reverse= True)

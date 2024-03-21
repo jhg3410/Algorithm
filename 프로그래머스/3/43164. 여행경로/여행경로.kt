935f05b3b26b976b9e49b178188bc6ccb053cc66
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-03-21 18:14:35 +0900

    [level 3] Title: 여행경로, Time: 1.83 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.kt
@@ -0,0 +1,45 @@
+class Solution {
+    var ticketSize = 0
+    var answer = listOf<String>()
+    lateinit var tickets: Array<Array<String>>
+    fun solution(_tickets: Array<Array<String>>): Array<String> {
+        tickets = _tickets
+        ticketSize = tickets.size
+
+        dfs(start = "ICN", visitedIdxArray = BooleanArray(size = ticketSize), visitedContry = listOf("ICN"))
+
+        return answer.toTypedArray()
+    }
+
+    private fun dfs(start: String, visitedIdxArray: BooleanArray, visitedContry: List<String>) {
+        if (visitedContry.size == ticketSize + 1) {
+            if (answer.size == 0) {
+                answer = visitedContry
+                return
+            }
+            var v = ""
+            var a = ""
+            visitedContry.forEach {
+                v += it
+            }
+            answer.forEach {
+                a += it
+            }
+            if (v < a) {
+                answer = visitedContry
+            }
+        }
+
+        for (i in tickets.indices) {
+            if (visitedIdxArray[i]) continue
+            if (tickets[i][0] == start) {
+                visitedIdxArray[i] = true
+                dfs(
+                    start = tickets[i][1], visitedIdxArray = visitedIdxArray, visitedContry =
+                    visitedContry.plus(tickets[i][1])
+                )
+                visitedIdxArray[i] = false
+            }
+        }
+    }
+}

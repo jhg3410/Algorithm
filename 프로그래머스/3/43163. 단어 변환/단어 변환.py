ebf2cf4d52a0f3946524bbf5bcf2efce01aa20eb
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-03-21 17:15:43 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -10,11 +10,7 @@ def solution(begin, target, words):
         if current == target: return count
         for word in words:
             if word in visited: continue
-            diffCount = 0
-            for w, c  in zip(word, current):
-                if c != w:
-                    diffCount +=1
-            if diffCount == 1:
+            if sum([w!=c for w, c  in zip(word, current)]) == 1:
                 queue.append([word, count+1])
                 visited.append(word)
 

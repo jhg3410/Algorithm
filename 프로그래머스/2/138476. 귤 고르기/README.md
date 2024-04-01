ead1033f8706502dadc700aae8e42fec8220aaa6
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-04-01 17:04:46 +0900

    [level 2] Title: 귤 고르기, Time: 26.81 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 25.6 MB, 시간: 43.40 ms
+메모리: 18.1 MB, 시간: 26.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 01일 17:03:32
+2024년 04월 01일 17:04:45
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -8,8 +8,8 @@ def solution(k, tangerines):
 
     for tangerine in tangerines:
         tan_cnt[tangerine] += 1
-    sorted_tan_cnt = {x: y for x, y in sorted(tan_cnt.items(), key=lambda x: x[1], reverse=True)}
-    for idx, tan_cnt in enumerate(sorted_tan_cnt.values()):
+    sorted_tan_cnt = sorted(tan_cnt.values(), reverse=True)
+    for idx, tan_cnt in enumerate(sorted_tan_cnt):
         limit -= tan_cnt
         if limit <= 0:
             return idx + 1

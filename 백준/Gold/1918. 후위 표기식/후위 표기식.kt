0ba0f0fc8e863ed9b6cffe341138588d1b2c00a4
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-06-02 00:07:29 +0900

    [Gold II] Title: 후위 표기식, Time: 128 ms, Memory: 21484 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.kt
@@ -0,0 +1,58 @@
+package heejik.`65week`
+
+private class `후위 표기식` {
+
+    val stack = mutableListOf<String>()
+    val sb = StringBuilder()
+
+    fun solve() {
+        val expression = readln().toList().map { it.toString() }
+        expression.forEach {
+            if (it !in "+-*/()") {
+                sb.append(it)
+                return@forEach
+            }
+            if (it == "(") {
+                stack.add(it)
+                return@forEach
+            }
+            if (it == ")") {
+                while (true) {
+                    val last = stack.removeLast()
+                    if (last == "(") {
+                        break
+                    } else {
+                        sb.append(last)
+                    }
+                }
+            }
+            if (it in "*/") {
+                while (true) {
+                    if (stack.isEmpty() || stack.last() in "(+-") {
+                        break
+                    }
+                    sb.append(stack.removeLast())
+                }
+                stack.add(it)
+                return@forEach
+            }
+            if (it in "+-") {
+                while (true) {
+                    if (stack.isEmpty() || stack.last() in "(") {
+                        break
+                    }
+                    sb.append(stack.removeLast())
+                }
+                stack.add(it)
+            }
+        }
+        while (stack.isNotEmpty()) {
+            sb.append(stack.removeLast())
+        }
+        println(sb.toString())
+    }
+}
+
+fun main() {
+    `후위 표기식`().solve()
+}

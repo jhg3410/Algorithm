7b4d396061e78cca6c4fdc29050dbaf9197a0ad8
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-06-09 21:07:52 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 164 ms, Memory: 23268 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.kt
@@ -0,0 +1,49 @@
+import kotlin.math.abs
+
+class 소병희_맥주마시며걸어가기 {
+    companion object {
+        fun solve() = with(System.`in`.bufferedReader()) {
+            val t = readLine().toInt()
+
+            repeat(t) {
+                val n = readLine().toInt()
+                val q = ArrayDeque<IntArray>()
+                val home = readLine().split(" ").let { intArrayOf(it[0].toInt(), it[1].toInt()) }
+                val conv = Array(n) { IntArray(2) }
+                val visited = BooleanArray(n)
+
+                for (i in 0 until n) {
+                    conv[i] = readLine().split(" ").let { intArrayOf(it[0].toInt(), it[1].toInt()) }
+                }
+
+                val festival = readLine().split(" ").let { intArrayOf(it[0].toInt(), it[1].toInt()) }
+
+                q.add(intArrayOf(home[0], home[1]))
+                var possible = false
+
+                while (q.isNotEmpty()) {
+                    val (x, y) = q.removeFirst()
+                    if (abs(x - festival[0]) + abs(y - festival[1]) <= 1000) {
+                        possible = true
+                        break
+                    }
+
+                    for (i in 0 until n) {
+                        if (visited[i]) continue
+
+                        val dist = abs(x - conv[i][0]) + abs(y - conv[i][1])
+                        if (dist > 1000) continue
+
+                        visited[i] = true
+                        q.add(intArrayOf(conv[i][0], conv[i][1]))
+                    }
+                }
+                println(if (possible) "happy" else "sad")
+            }
+        }
+    }
+}
+
+fun main() {
+    소병희_맥주마시며걸어가기.solve()
+}

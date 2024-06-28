f7856b37a4ffbc0930622eeb642df5c03b3b66cc
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-06-29 00:55:18 +0900

    [Silver II] Title: N번째 큰 수, Time: 1436 ms, Memory: 370980 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.kt
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.kt
@@ -1,38 +1,25 @@
-package heejik.`51week`
+import java.io.*
+import java.util.PriorityQueue
 
-import java.util.*
-
-class `N번째 큰 수` {
-
-    data class Number(
-        val x: Int,
-        val y: Int,
-        val value: Long
-    )
-
-    fun solve() {
-        val n = readln().toInt()
-        val numbers = PriorityQueue<Number>(compareByDescending { it.value })
-        val board = MutableList(n) { MutableList(n) { 0L } }
-
-        repeat(n) { x ->
-            readln().split(' ').map { it.toLong() }.forEachIndexed { y, l ->
-                board[x][y] = l
-                if (x == n - 1) {
-                    numbers.add(Number(x, y, l))
-                }
-            }
-        }
-
-        repeat(n - 1) {
-            val max = numbers.poll()
-            numbers.add(Number(max.x - 1, max.y, board[max.x - 1][max.y]))
-        }
-        println(numbers.peek().value)
-    }
-}
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+private fun readln() = br.readLine()
 
 
 fun main() {
-    `N번째 큰 수`().solve()
+    val numbers = PriorityQueue<Int> { number1, number2 ->
+        number2 - number1
+    }
+    
+    val n = readln().toInt()
+    repeat(n) {
+        readln().split(" ").map{ it.toInt() }.forEach { num ->
+            numbers.add(num)
+        }
+    }
+
+    repeat(n-1) {
+        numbers.poll()
+    }
+    println(numbers.poll())
 }

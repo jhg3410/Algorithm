f57b67786803cd01528e33dff96cd9e7dea3a5b7
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-08-21 20:51:17 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 48.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,48 +1,30 @@
 from collections import deque
 
-n = -1
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-
-def in_range(x: int, y: int):
-    return x in range(n) and y in range(n)
-
-
-def find_cross(d: int):
-    return d % 2 == 0
-
 
 def solution(board):
-    global n
     n = len(board)
-    # cost_board[x][y][d] d == 0 이면 수직 d == 1 이면 수평
-    cost_board = [[[10 ** 9 for _ in range(2)] for _ in range(n)] for _ in range(n)]
-
-    queue = deque()
-    queue.append([0, 0])
-    cost_board[0][0][0] = 0
-
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if not in_range(nx, ny): continue
-            if board[nx][ny] == 1: continue
-            hor_ver = find_cross(i)
-            # 수직이면
-            if hor_ver == 1:
-                new_cost = min(cost_board[x][y][1] + 100, cost_board[x][y][0] + 600)
-                if x == 0 and y == 0: new_cost = 100
-                if new_cost >= cost_board[nx][ny][1]: continue
-                cost_board[nx][ny][1] = new_cost
-                queue.append([nx, ny])
-            else:
-                new_cost = min(cost_board[x][y][0] + 100, cost_board[x][y][1] + 600)
-                if new_cost >= cost_board[nx][ny][0]: continue
-                cost_board[nx][ny][0] = new_cost
-                queue.append([nx, ny])
-
-    return min(cost_board[n - 1][n - 1])
-
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
+    answer = float('inf')
+    q = deque()
+    q.append((0,0,-1,0))
+    visit = {(0,0,0):0, (0,0,1):0, (0,0,2):0, (0,0,3):0}
+    while q:
+        x, y, direction, cost =  q.popleft()
+        for d in range(4):
+            newcost = cost
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if -1 <nx < n and -1 < ny < n and board[nx][ny] != 1:
+                if direction == -1:
+                    newcost += 100
+                elif (direction - d) % 2 == 0:
+                    newcost += 100
+                else:
+                    newcost += 600
+                if nx == n-1 and ny == n-1:
+                    answer = min(answer, newcost)
+                elif visit.get((nx, ny, d)) is None or visit.get((nx,ny,d)) > newcost:
+                    visit[(nx, ny, d)] = newcost
+                    q.append((nx, ny, d, newcost))
+    return answer

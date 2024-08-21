afdd534d286d6bc855ed8568628f5d89e3bb6881
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-08-21 21:01:00 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 46.45 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -3,28 +3,35 @@ from collections import deque
 
 def solution(board):
     n = len(board)
-    dx = [-1,0,1,0]
-    dy = [0,1,0,-1]
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
     answer = float('inf')
     q = deque()
-    q.append((0,0,-1,0))
-    visit = {(0,0,0):0, (0,0,1):0, (0,0,2):0, (0,0,3):0}
+    q.append((0, 0, -1, 0))
+    visit = {(0, 0, 0): 0, (0, 0, 1): 0}
     while q:
-        x, y, direction, cost =  q.popleft()
+        x, y, direction, cost = q.popleft()
         for d in range(4):
             newcost = cost
             nx = x + dx[d]
             ny = y + dy[d]
-            if -1 <nx < n and -1 < ny < n and board[nx][ny] != 1:
+            if -1 < nx < n and -1 < ny < n and board[nx][ny] != 1:
                 if direction == -1:
                     newcost += 100
-                elif (direction - d) % 2 == 0:
-                    newcost += 100
+                elif direction % 2 == 0:
+                    if d % 2 == 0:
+                        newcost += 100
+                    else:
+                        newcost += 600
                 else:
-                    newcost += 600
-                if nx == n-1 and ny == n-1:
+                    if d % 2 == 0:
+                        newcost += 600
+                    else:
+                        newcost += 100
+                nd = 0 if d % 2 == 0 else 1
+                if nx == n - 1 and ny == n - 1:
                     answer = min(answer, newcost)
-                elif visit.get((nx, ny, d)) is None or visit.get((nx,ny,d)) > newcost:
-                    visit[(nx, ny, d)] = newcost
-                    q.append((nx, ny, d, newcost))
+                elif visit.get((nx, ny, nd)) is None or visit.get((nx, ny, nd)) > newcost:
+                    visit[(nx, ny, nd)] = newcost
+                    q.append((nx, ny, nd, newcost))
     return answer

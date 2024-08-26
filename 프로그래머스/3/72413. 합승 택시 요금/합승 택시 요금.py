3180cea33a168286fb5e3496e602128f940ace79
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-08-27 00:16:40 +0900

    [level 3] Title: 합승 택시 요금, Time: 81.90 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,48 +1,45 @@
+import heapq
+
 cant_go = 10 ** 10
 
 
 def solution(n, s, a, b, fares):
     answer = cant_go
 
-    costs = [[cant_go for _ in range(n + 1)] for _ in range(n + 1)]
+    min_costs = [[cant_go for _ in range(n + 1)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        min_costs[i][i] = 0
+
+    moves = [[] for _ in range(n + 1)]
     for c, d, f in fares:
-        costs[c][d] = f
-        costs[d][c] = f
+        moves[c].append([f, d])
+        moves[d].append([f, c])
+
+    def dijkstra(start: int):
+        pq = []
+        distances = [cant_go for _ in range(n + 1)]
+        distances[start] = 0
+        heapq.heappush(pq, [0, start])
+
+        while pq:
+            cumulate_cost, start_node = heapq.heappop(pq)
+            for cost, end_node in moves[start_node]:
+                new_cost = cumulate_cost + cost
+                if distances[end_node] <= new_cost: continue
+                heapq.heappush(pq, [new_cost, end_node])
+                distances[end_node] = new_cost
+                min_costs[start][end_node] = new_cost
+
+    dijkstra(s)
+    dijkstra(a)
+    dijkstra(b)
+
     for i in range(1, n + 1):
-        costs[i][i] = 0
+        answer = min(answer, min_costs[s][i] + min_costs[a][i] + min_costs[b][i])
 
-    # a 에서 모든 좌표까지의 최단 거리
-    for k in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                if costs[i][k] == cant_go or costs[k][j] == cant_go: continue
-                costs[i][j] = min(costs[i][j], costs[i][k] + costs[k][j])
-
-    # for i in range(a, a + 1):
-    #     for j in range(1, n + 1):
-    #         for k in range(1, n + 1):
-    #             if costs[i][j] == cant_go or costs[j][k] == cant_go: continue
-    #             costs[i][k] = min(costs[i][k], costs[i][j] + costs[j][k])
-    # # b 에서 모든 좌표까지의 최단 거리
-    # for i in range(b, b + 1):
-    #     for j in range(1, n + 1):
-    #         for k in range(1, n + 1):
-    #             if costs[i][j] == cant_go or costs[j][k] == cant_go: continue
-    #             costs[i][k] = min(costs[i][k], costs[i][j] + costs[j][k])
-    # # s 에서 모든 좌표까지의 최단 거리
-    # for i in range(s, s + 1):
-    #     for j in range(1, n + 1):
-    #         for k in range(1, n + 1):
-    #             if costs[i][j] == cant_go or costs[j][k] == cant_go: continue
-    #             costs[i][k] = min(costs[i][k], costs[i][j] + costs[j][k])
-    for i in range(1, n + 1):
-        if costs[s][i] == cant_go or costs[a][i] == cant_go or costs[b][i] == cant_go: continue
-        answer = min(answer, costs[s][i] + costs[a][i] + costs[b][i])
-
-    # print(costs)
     return answer
 
 
 if __name__ == '__main__':
-    solution(7, 3, 4, 1, fares=[[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]])
+    print(solution(7, 3, 4, 1, fares=[[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]]))
     # print(solution(3, 2, 1, 3, fares=[[1, 2, 10], [3, 1, 11]]))

c6ea578b5978fd741f0e4b15d99687630b22b1d3
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-09-02 20:46:07 +0900

    [level 3] Title: 길 찾기 게임, Time: 176.60 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,11 +1,19 @@
 from sys import setrecursionlimit
-setrecursionlimit(10**6)
+
+setrecursionlimit(10 ** 6)
 
 node_info = []
 answer = [[], []]
 
 
-def find_answer(nodes):
+class Node:
+    def __init__(self, number, left=None, right=None):
+        self.number = number
+        self.left = left
+        self.right = right
+
+
+def make_tree(nodes):
     if not nodes: return
     # 루트 찾기(y, idx, number)
     root = (-1, -1, -1)
@@ -15,22 +23,33 @@ def find_answer(nodes):
     root_idx = root[1]
     root_number = root[2]
 
-    answer[0].append(root_number)
     # 루트 왼쪽, 오른쪽 분리
     lefts, rights = nodes[:root_idx], nodes[root_idx + 1:]
 
-    find_answer(lefts)
+    return Node(root_number, left=make_tree(lefts), right=make_tree(rights))
 
-    find_answer(rights)
-    answer[1].append(root_number)
+
+def preorder(root_node: Node):
+    if root_node is None: return
+    answer[0].append(root_node.number)
+    preorder(root_node.left)
+    preorder(root_node.right)
+
+
+def postorder(root_node: Node):
+    if root_node is None: return
+    postorder(root_node.left)
+    postorder(root_node.right)
+    answer[1].append(root_node.number)
 
 
 def solution(_nodeinfo):
     global node_info
-
     node_info = sorted([[x, y, number + 1] for number, (x, y) in enumerate(_nodeinfo)])
+    root_node = make_tree(node_info)
 
-    find_answer(nodes=node_info)
+    preorder(root_node=root_node)
+    postorder(root_node=root_node)
     return answer
 
 

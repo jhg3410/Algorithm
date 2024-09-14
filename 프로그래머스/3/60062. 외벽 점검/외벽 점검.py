1d3ee39f0a005dd956c258fd329ec679196bba3f
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-09-14 10:55:16 +0900

    [level 3] Title: 외벽 점검, Time: 2435.18 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
@@ -0,0 +1,58 @@
+from itertools import permutations
+
+n, weaks, dists = 0, [], []
+weaks_count = -1
+friend_count = -1
+answer = 10 ** 10
+real_weak = []
+
+
+def solution(_n, _weak, _dist):
+    global n, weaks, dists, weaks_count, friend_count, real_weak, answer
+    n, weaks, dists = _n, _weak.copy(), sorted(_dist, reverse=True)
+
+    real_weak = _weak.copy()
+    weaks_count = len(weaks)
+    friend_count = len(dists)
+
+    for weak in _weak:
+        weaks.append(weak + n)
+
+    for i in range(1, len(dists) + 1):
+        for case in get_all_case([], i):
+            if check(case):
+                return i
+
+    return -1
+
+
+def get_all_case(li, count):
+    if len(li) == count:
+        return [list(map(lambda x: dists[x], li))]
+
+    tmp = []
+    for i in range(len(dists)):
+        if i in li: continue
+        li.append(i)
+        tmp.extend(get_all_case(li, count))
+        li.pop()
+
+    return tmp
+
+
+def check(case):
+    global answer
+    for i in range(weaks_count):
+        new_weaks = weaks[i: i + weaks_count]
+        idx = 0
+        for dist in case:
+            bound = new_weaks[idx] + dist
+            while bound >= new_weaks[idx]:
+                idx += 1
+                if idx == weaks_count:
+                    return True
+    return False
+
+
+if __name__ == '__main__':
+    print(solution(_n=16, _weak=[1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 14, 15], _dist=[4, 2, 1, 1]))

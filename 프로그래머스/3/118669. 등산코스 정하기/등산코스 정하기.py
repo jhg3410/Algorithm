c845ee57f154911afdbd811b56f1823a2ba8898a
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-09-16 15:09:51 +0900

    [level 3] Title: 등산코스 정하기, Time: 810.21 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -0,0 +1,69 @@
+import heapq
+
+n = -1
+undefined = 10 ** 10
+top_number = -1
+min_intensity = undefined
+relations = []
+summited = []
+gated = []
+queue = []
+
+
+def bfs():
+    global min_intensity, top_number
+    visited = [undefined for _ in range(n + 1)]
+
+    while queue:
+        max_dist, number, start = heapq.heappop(queue)
+        if gated[number]:
+
+            # print(f'start: {start}, number: {number}, max_dist: {max_dist}, visited[number]: {visited[number]}')
+            if max_dist < min_intensity:
+                top_number, min_intensity = start, max_dist
+            elif max_dist == min_intensity:
+                top_number = min(top_number, start)
+            continue
+
+        for can_go, dist in relations[number]:
+            # print(f'can_go: {can_go}, dist: {dist}, max_dist:{max_dist}')
+            new_dist = max(dist, max_dist)
+            if new_dist >= visited[can_go]: continue
+            if dist > min_intensity: continue
+            heapq.heappush(queue, (new_dist, can_go, start))
+            if not gated[can_go]:
+                visited[can_go] = new_dist
+
+def solution(_n, paths, gates, summits):
+    global n, relations, summited, gated, queue
+    n = _n
+    relations = [[] for _ in range(n + 1)]
+    summited = [False for _ in range(n + 1)]
+    gated = [False for _ in range(n + 1)]
+
+    for summit in summits:
+        summited[summit] = True
+
+    for gate in gates:
+        gated[gate] = True
+
+    for number1, number2, distance in paths:
+        if summited[number1]:
+            relations[number1].append([number2, distance])
+        elif summited[number2]:
+            relations[number2].append([number1, distance])
+        else:
+            relations[number1].append([number2, distance])
+            relations[number2].append([number1, distance])
+
+    for summit in summits:
+        heapq.heappush(queue, (-1, summit, summit))
+
+    bfs()
+
+    return top_number, min_intensity
+
+
+if __name__ == '__main__':
+    print(solution(_n=6, paths=[[1, 2, 3], [2, 3, 5], [2, 4, 2], [2, 5, 4], [3, 4, 4], [4, 5, 3], [4, 6, 1], [5, 6, 1]]
+                   , gates=[1, 3], summits=[5]))

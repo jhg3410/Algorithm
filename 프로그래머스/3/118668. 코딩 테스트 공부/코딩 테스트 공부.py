f5b1382df47bd91a14d88db7fd6b6e00b991baba
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-09-18 21:27:42 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 320.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -4,31 +4,34 @@ LIMIT = 150
 answer = 10 ** 10
 
 
-def solution(alp, cop, problems):
+def solution(_alp, _cop, problems):
     global answer
     need_alp = max(list(map(lambda x: x[0], problems)))
     need_cop = max(list(map(lambda x: x[1], problems)))
     # 기본 증가 추가(1에 1시간)
     problems.append([0, 0, 1, 0, 1])
     problems.append([0, 0, 0, 1, 1])
-    queue = []
-    # 소요 시간, 알고력, 코딩력
-    heapq.heappush(queue, [0, alp, cop])
-    visited = [[10 ** 10 for _ in range(LIMIT + 1)] for _ in range(LIMIT + 1)]
-    visited[alp][cop] = 0
 
-    while queue:
-        time, c_alp, c_cop = heapq.heappop(queue)
-        if c_alp >= need_alp and c_cop >= need_cop:
-            return time
-        for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
-            if c_alp >= alp_req and c_cop >= cop_req:
-                new_alp, new_cop = min(LIMIT, c_alp + alp_rwd), min(LIMIT, c_cop + cop_rwd)
-                new_time = time + cost
-                if new_time >= visited[new_alp][new_cop]: continue
-                visited[new_alp][new_cop] = time + cost
-                heapq.heappush(queue, [time + cost, new_alp, new_cop])
+    start_alp = _alp
+    start_cop = _cop
+
+    need_alp = max(start_alp, need_alp)
+    need_cop = max(start_cop, need_cop)
+    
+    dp = [[10 ** 10 for _ in range(need_cop + 1)] for _ in range(need_alp + 1)]
+    dp[start_alp][start_cop] = 0
+  
+
+    for alp in range(start_alp, need_alp + 1):
+        for cop in range(start_cop, need_cop + 1):
+            for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+                if alp >= alp_req and cop >= cop_req:
+                    new_alp, new_cop = min(need_alp, alp + alp_rwd), min(need_cop, cop + cop_rwd)
+
+                    dp[new_alp][new_cop] = min(dp[new_alp][new_cop], dp[alp][cop] + cost)
+
+    return dp[-1][-1]
 
 
 if __name__ == '__main__':
-    print(solution(alp=10, cop=10, problems=[[10, 15, 2, 1, 2], [20, 20, 3, 3, 4]]))
+    print(solution(_alp=0, _cop=0, problems=[[0, 0, 2, 1, 2], [4, 5, 3, 1, 2], [4, 11, 4, 0, 2], [10, 4, 0, 4, 2]]))

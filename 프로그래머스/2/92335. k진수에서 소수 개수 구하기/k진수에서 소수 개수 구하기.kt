11fabd4d7c42601e1ef582271e991fc576feb823
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-10-05 01:35:09 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 27.47 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.kt
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.kt
@@ -1,42 +1,24 @@
 import kotlin.math.sqrt
 
+
 class Solution {
     fun solution(n: Int, k: Int): Int {
-        var answerCount = 0
 
-        n.changeToKNumber(k).split('0').forEach {
-            it.toLongOrNull()?.let {
-                if (it.isPrimeNumber()) answerCount++
-            }
+        return changeToNumber(number = n, k = k).split('0').count {
+            (it.isNotEmpty() && isPrime(it.toLong()))
         }
-
-        return answerCount
     }
 
-
-    private fun Int.changeToKNumber(k: Int): String {
-        var tmp = this
-        val sb = StringBuilder()
-
-        while (tmp >= k) {
-            sb.insert(0, tmp % k)
-            tmp /= k
-        }
-        sb.insert(0, tmp)
-        return sb.toString()
+    private fun changeToNumber(number: Int, k: Int): String {
+        return number.toString(k)
     }
 
-    private fun Long.isPrimeNumber(): Boolean {
-
-
-        if (this == 1L) return false
-        if (this == 2L) return true
-        if (this % 2 == 0.toLong()) return false
-
-        for (i in 3..sqrt(this.toDouble()).toInt()) {
-            if (this % i == 0.toLong()) return false
+    private fun isPrime(number: Long): Boolean {
+        if (number == 1L) return false
+        if (number == 2L) return true
+        for (i in 2..sqrt(number.toDouble()).toInt() + 1) {
+            if (number % i == 0L) return false
         }
-
         return true
     }
 }

c4a89c8c9fb79bde49689ea852ece8de92899bf4
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-10-25 00:42:11 +0900

    [level 3] Title: 카드 짝 맞추기, Time: 1679.47 ms, Memory: 370 MB -BaekjoonHub

--- 프로그래머스/3/72415. 카드 짝 맞추기/카드 짝 맞추기.kt
+++ 프로그래머스/3/72415. 카드 짝 맞추기/카드 짝 맞추기.kt
@@ -1,15 +1,5 @@
 import kotlin.math.min
 
-
-data class Manipulation(
-    val x: Int,
-    val y: Int,
-    val board: List<List<Int>>,
-    val controlCount: Int,
-    val removeCount: Int,
-    val reverseCard: Int        // 0이면 뒤집힌 게 없다.
-)
-
 private fun List<List<Int>>.deepCopy(): List<MutableList<Int>> {
     val newList = List(size = 4) { MutableList(size = 4) { 0 } }
 
@@ -40,33 +30,28 @@ class Solution {
 
         for (i in 0..3) {
             for (j in 0..3) {
-                if (board[i][j] > 0) {
+                val number = board[i][j]
+                if (number > 0) {
                     originBoard[i][j] = board[i][j]
 
-                    val number = board[i][j]
                     if (number in cards) continue
                     cards.add(number)
                 }
             }
         }
 
-        getCase(stored = mutableListOf<Int>())
+        getCase(stored = mutableListOf())
 
         return answer
     }
 
-    // 모든 케이스 (순서 X, 중복 X)
     private fun getCase(stored: MutableList<Int>) {
-        // 케이스를 구함
         if (stored.size == cards.size) {
-//            println(stored)
             answer = min(answer, getControlCount(orders = stored))
             return
         }
 
-
-        for (i in cards.indices) {
-            val card = cards[i]
+        for (card in cards) {
             if (card in stored) continue
 
             stored.add(card)
@@ -80,42 +65,51 @@ class Solution {
         return move(orders, startX, startY, 0, false, board)
     }
 
-    private fun move(findNumbers:List<Int>, currentX : Int, currentY : Int, ctrlCount: Int, isReversed: Boolean, board: List<MutableList<Int>>): Int {
-//        println("findNumbers: $findNumbers")
+    private fun move(
+        findNumbers: List<Int>,
+        currentX: Int,
+        currentY: Int,
+        ctrlCount: Int,
+        isReversed: Boolean,
+        board: List<MutableList<Int>>
+    ): Int {
         var minCount = Int.MAX_VALUE
-        if (findNumbers.isEmpty()) {
-//            println("ctrlCount: $ctrlCount")
-            return ctrlCount
-        }
+        if (findNumbers.isEmpty()) return ctrlCount
 
         val findNumber = findNumbers.first()
+        val newBoard = board.deepCopy()
 
         // 이미 찾는 카드 하나가 뒤집혀 있으면
         if (isReversed) {
-            val newBoard = board.deepCopy()
             val (nx, ny, dist) = getMinDistance(currentX, currentY, newBoard, findCard = findNumber)
             newBoard[nx][ny] = 0
             minCount = min(minCount, move(findNumbers.drop(1), nx, ny, ctrlCount + dist + 1, false, newBoard))
         } else {
-            val newBoard = board.deepCopy()
-
             val (firstX, firstY, firstDist) = getMinDistance(currentX, currentY, newBoard, findCard = findNumber)
             newBoard[firstX][firstY] = 0
             val (secondX, secondY, secondDist) = getMinDistance(currentX, currentY, newBoard, findCard = findNumber)
 
-            minCount = min(minCount, move(findNumbers.toList(), firstX, firstY, ctrlCount + firstDist + 1, true, newBoard))
+            minCount =
+                min(minCount, move(findNumbers.toList(), firstX, firstY, ctrlCount + firstDist + 1, true, newBoard))
             val newNewBoard = board.deepCopy()
             newNewBoard[secondX][secondY] = 0
-            minCount = min(minCount, move(findNumbers.toList(), secondX, secondY, ctrlCount + secondDist + 1, true, newNewBoard))
+            minCount = min(
+                minCount,
+                move(findNumbers.toList(), secondX, secondY, ctrlCount + secondDist + 1, true, newNewBoard)
+            )
         }
 
         return minCount
     }
 
 
-
     // 특정 카드로 가는 최단 길
-    private fun getMinDistance(currentX: Int, currentY: Int, board: List<List<Int>>, findCard: Int): Triple<Int, Int, Int> {
+    private fun getMinDistance(
+        currentX: Int,
+        currentY: Int,
+        board: List<List<Int>>,
+        findCard: Int
+    ): Triple<Int, Int, Int> {
 
         var minX = -1
         var minY = -1
@@ -126,7 +120,6 @@ class Solution {
         queue.add(Triple(currentX, currentY, 0))
 
         while (queue.isNotEmpty()) {
-
             val (x, y, moveCount) = queue.removeFirst()
             if (board[x][y] == findCard) {
                 minX = x
@@ -152,17 +145,6 @@ class Solution {
                 }
             }
         }
-//        println("currentX: $currentX")
-//        println("currentY: $currentY")
-//        for (row in board) {
-//            println(row)
-//        }
-//        println("findCard: $findCard")
-//        println("minX: $minX")
-//        println("minY: $minY")
-//        println("minDistance: $minDistance")
-//        println("------------------------------------------")
-
         return Triple(minX, minY, minDistance)
     }
 
@@ -170,7 +152,6 @@ class Solution {
         direction: Int, x: Int, y: Int,
         board: List<List<Int>>
     ): Pair<Int, Int> {
-
         var nx = x
         var ny = y
         while (true) {
@@ -187,18 +168,5 @@ class Solution {
         }
 
         return nx to ny
-    }
-}
-
-fun main() {
-    Solution().solution(
-        board = arrayOf(
-            intArrayOf(1, 0, 0, 3),
-            intArrayOf(2, 0, 0, 0),
-            intArrayOf(0, 0, 0, 2),
-            intArrayOf(3, 0, 1, 0)
-        ), r = 1, c = 0
-    ).also {
-        println(it)
     }
 }

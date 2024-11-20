2112ff9ef4e1bbd479de0465cd5a6bc8aa64dcce
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2024-11-20 22:13:13 +0900

    [level 3] Title: 풍선 터트리기, Time: 352.65 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,20 +1,19 @@
 def solution(a):
     answer = 0
     counts = [0 for _ in range(len(a))]
-    
+
     left_min = a[0]
     for i in range(1, len(a)):
         if a[i] > left_min:
             counts[i] += 1
         else:
             left_min = a[i]
-            
+
     right_min = a[-1]
-    for i in range(len(a)-2, -1, -1):
+    for i in range(len(a) - 2, -1, -1):
         if a[i] > right_min:
             counts[i] += 1
         else:
             right_min = a[i]
-    
-    
-    return len(list(filter(lambda x: x!=2, counts)))
+
+    return len(counts) - counts.count(2)
